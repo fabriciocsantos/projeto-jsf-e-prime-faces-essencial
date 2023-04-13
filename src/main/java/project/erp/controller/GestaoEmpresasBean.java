3e2bca5748c6a24faf5ceba45aabc9a79679bd58
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-04-13 11:02:50 -0300

    Listando empresas com tabela de dados
    Começo da criação do projeto com lista de retorno para todas as empresas

--- src/main/java/project/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/project/erp/controller/GestaoEmpresasBean.java
@@ -1,15 +1,31 @@
 package project.erp.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import project.erp.model.Empresa;
+import project.erp.repository.Empresas;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
     
-   
+    @Inject
+    private Empresas empresas;
+    
+    private List<Empresa> listaEmpresas;
+    
+    public void todasEmpresas() {
+        listaEmpresas = empresas.todas();
+    }
+    
+    public List<Empresa> getListaEmpresas() {
+        return listaEmpresas;
+    }
 }
--- src/main/java/project/erp/repository/Empresas.java
+++ src/main/java/project/erp/repository/Empresas.java
@@ -38,6 +38,10 @@ public class Empresas implements Serializable {
 		
 		return query.getResultList();
 	}
+	
+	public List<Empresa> todas() {		
+		return manager.createQuery("from Empresa", Empresa.class).getResultList();
+	}
 
 	public Empresa guardar(Empresa empresa) {
 		return manager.merge(empresa);
