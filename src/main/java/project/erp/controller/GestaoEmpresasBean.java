ac324a59750715f166a41e8b58a0c10a148f40e5
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-04-13 08:59:37 -0300

    Exclusão de dados
    Exclusão de dados que foram feitos para aprendizado, inicio do projeto real

--- src/main/java/project/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/project/erp/controller/GestaoEmpresasBean.java
@@ -5,32 +5,11 @@ import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import project.erp.model.Empresa;
-import project.erp.model.TipoEmpresa;
-
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
-    private Empresa empresa = new Empresa();
-    
-    public void salvar() {
-    	System.out.println("Razão Social: " + empresa.getRazaoSocial()
-    			+ " - Nome Fantasia: " + empresa.getNomeFantasia()
-    			+ " - Tipo: " + empresa.getTipo());
-    }
-    
-    public String ajuda() {
-    	return "AjudaGestaoEmpresas?faces-redirect=true";
-    }
-    
-    public Empresa getEmpresa() {
-		return empresa;
-	} 
-    
-    public TipoEmpresa[]getTiposEmpresa(){
-    	return TipoEmpresa.values();
-    }
+   
 }
