12e0247afb4d590c81a929f4c086749a117189f9
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-04-17 11:58:33 -0300

    Atualização ERRO
    RESOLVIDO, modificações em classes, Persistence e Adicionado dependencias no pom.xml. Alguns problemas de versionamento

--- src/main/java/project/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/project/erp/controller/GestaoEmpresasBean.java
@@ -3,6 +3,7 @@ package project.erp.controller;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -20,6 +21,11 @@ public class GestaoEmpresasBean implements Serializable {
     private Empresas empresas;
     
     private List<Empresa> listaEmpresas;
+    
+    public void listenerPreRenderView() {
+    	listaEmpresas = empresas.todas();
+    	System.out.println("Teste");
+    }
     
     public void todasEmpresas() {
        listaEmpresas = empresas.todas();
