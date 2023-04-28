fc0a642744e7d06300f403a19fba0af5611ee31f
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-04-28 11:22:57 -0300

    Edição
    Implementando a edição de registros

--- src/main/java/project/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/project/erp/controller/GestaoEmpresasBean.java
@@ -50,6 +50,10 @@ public class GestaoEmpresasBean implements Serializable {
     	empresa = new Empresa();
     }
     
+    public void prepararEdicao() {
+    	ramoAtividadeConverter = new RamoAtividadeConverter(Arrays.asList(empresa.getRamoAtividade()));
+    }
+    
     public void salvar() {
     	cadastroEmpresasService.salvar(empresa);
     	
