6bd2bf1115e4eea7e17ea180b1900581c0c031ac
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-05-30 11:14:35 -0300

    Login
    Acertando tela de login, messages e credenciais corretas para uso

--- src/main/java/project/erp/controller/PerfilUsuarioBean.java
+++ src/main/java/project/erp/controller/PerfilUsuarioBean.java
@@ -5,7 +5,10 @@ import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import project.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
@@ -13,10 +16,14 @@ public class PerfilUsuarioBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private FacesMessages messages;
+
 	private String login;
-	private String senha;	
+	private String senha;
+
 	FacesMessage msg = null;
-	
+
 	public void tempoEspera() {
 		// Simula demora no processamento
 		try {
@@ -28,29 +35,35 @@ public class PerfilUsuarioBean implements Serializable {
 
 	public void verificarDisponibilidadeLogin() {
 		tempoEspera();
-		
-		if ("joao".equalsIgnoreCase(this.login)) {
-			msg = new FacesMessage("Login já está em uso.");
-			msg.setSeverity(FacesMessage.SEVERITY_WARN);
+
+		if ("Admin".equalsIgnoreCase(this.login)) {
+			msg = new FacesMessage("Login correto.");
+			msg.setSeverity(FacesMessage.SEVERITY_INFO);
 		} else {
-			msg = new FacesMessage("Login disponível.");
+			msg = new FacesMessage("Login incorreto.");
+			msg.setSeverity(FacesMessage.SEVERITY_WARN);
 		}
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
-	
-	
-	public void cadastrar() {	
-		verificarDisponibilidadeLogin();
-		
-		if ("joao".equalsIgnoreCase(this.login)) {
-			msg = new FacesMessage("Login já está em uso.");
+
+	public void cadastrar() {
+		tempoEspera();
+
+		if ("Admin".equalsIgnoreCase(this.login)) {
+			if ("Admin".equalsIgnoreCase(this.senha)) {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Acesso Permitido!"));
+				System.out.println("Login: " + this.login + " ----- " + "Senha: " + this.senha + "--PERMITIDO--");
+			} else {
+				FacesContext.getCurrentInstance().addMessage(null, msg = new FacesMessage("Credenciais incorretas!"));
+				msg.setSeverity(FacesMessage.SEVERITY_WARN);
+				System.out.println("Login: " + this.login + " ----- " + "Senha: " + this.senha + "--NEGADO--");
+			}
+		}else {
+			msg = new FacesMessage("Login incorreto!");
 			msg.setSeverity(FacesMessage.SEVERITY_WARN);
-		} else {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cadastro efetuado!"));
-			System.out.println("Login: " + this.login + " ----- " + "Senha: " + this.senha);
 		}
 	}
-	
+
 	public String getLogin() {
 		return login;
 	}
@@ -65,6 +78,14 @@ public class PerfilUsuarioBean implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public FacesMessages getMessages() {
+		return messages;
+	}
+
+	public void setMessages(FacesMessages messages) {
+		this.messages = messages;
 	}
 
 }
